0440f44aef2de5bdbc1b3755bfba918a827cb5a7
Swati Jain <jain.swati71@gmail.com>
2024-02-21 21:14:41 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,42 @@ import SiriImage from './images/siri.png';
 function App() {
     return (
     <div>
-        <div> Personal Digital Assistants</div>
+        <section className="hero is-primary">
+            <div className="hero-body">
+                <p className="title">Personal Digital Assistants</p>
+            </div>
+        </section>
+
+        <div className="container">
+            <section className="section">
+                <div className="columns">
+                    <div className="column is-3">
+                    <ProfileCard title="Alexa" 
+                    handle="@alexa90" 
+                    image={AlexaImage}
+                    description="Alexa was created by Amazon."
+                    />
+                    </div>
+                    <div className="column is-3">
+                    <ProfileCard title="Cortana"
+                    handle="@cortana99" 
+                    image={CortanaImage}
+                    description="Cortana was created by Microsoft."
+                    />
+                    </div>
+                    <div className="column is-3">
+                    <ProfileCard title="Siri" 
+                    handle="@siri99" 
+                    image={SiriImage}
+                    description="Siri was created by Apple."
+                    />
+                    </div>
+                </div>
+            </section>
+        </div>
 
 
-        <ProfileCard title="Alexa" handle="@alexa90" image={AlexaImage}/>
-        <ProfileCard title="Cortana" handle="@cortana99" image={CortanaImage}/>
-        <ProfileCard title="Siri" handle="@siri99" image={SiriImage}/>
+
     </div>
     );
 }
